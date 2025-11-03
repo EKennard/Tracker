c2207f91b298553edb3a38951e3d34895b5304b2
EKennard <lizz.kennard@hotmail.com>
2025-11-03 19:12:21 +0000

    feat: add app description and copyright footer to all pages

--- getLizzyFit/views.py
+++ getLizzyFit/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from datetime import datetime
 
 def home(request):
-    return render(request, 'base.html')
+    return render(request, 'base.html', {'now': datetime.now()})
