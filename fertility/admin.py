b2abb6c9fa20f9aaaeb6f5849e2168ea391392b6
EKennard <lizz.kennard@hotmail.com>
2025-11-03 20:44:09 +0000

    Fix fertility app: add __init__.py, tests.py, register FertilityLog in admin, and create migrations

--- fertility/admin.py
+++ fertility/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Cycle, Fertility, Pregnancy
+from .models import Cycle, Fertility, Pregnancy, FertilityLog
 
 admin.site.register(Cycle)
 admin.site.register(Fertility)
 admin.site.register(Pregnancy)
+admin.site.register(FertilityLog)
