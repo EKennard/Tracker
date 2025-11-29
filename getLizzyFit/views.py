c7f8ecb1c85e5faa3b9af60c694ea3e3b73a7638
EKennard <lizz.kennard@hotmail.com>
2025-11-29 17:43:42 +0000

    Fix: Redirect authenticated users from home to dashboard

--- getLizzyFit/views.py
+++ getLizzyFit/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from datetime import datetime
 
 def home(request):
+    if request.user.is_authenticated:
+        return redirect('dashboard')
     return render(request, 'base.html', {'now': datetime.now()})
